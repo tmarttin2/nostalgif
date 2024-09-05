9e1e4e21de71d5c8510f06543c933f01545d2ade
Tiagoo! <163489030+tmarttin2@users.noreply.github.com>
2024-09-05 17:33:14 -0300

    Add files via upload

---
+++ TiagoMartins/footer.php
@@ -0,0 +1,11 @@
+            </div>
+        </div>
+    </div>
+
+    <!-- Rodapé do Sistema -->
+    <div class="mt-5 bg-dark text-white text-center fixed-bottom">
+        <p>NostalgIF - Tiago,Thayse e Victor</p>
+    </div>
+
+</body>
+</html>
